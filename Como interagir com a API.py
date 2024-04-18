a207680365a8423c9736977b854513cce24e78fc
JoaoRibeiro01 <158493175+Joaopydev@users.noreply.github.com>
2024-04-18 08:44:14 -0300

    info api

--- Como interagir com a API.py
+++ Como interagir com a API.py
@@ -1,10 +1,32 @@
 import requests
 
-# URL da sua API
-url = 'http://127.0.0.1:5000/adicionar_receita'
+#Adicionando receita
+url = 'http://127.0.0.1:5000/adicionar-receita'
 
 # Dados que você quer enviar para a API
-dados = {'valor': 2000}
+dados = {'valor': 3000}
+
+# Fazendo uma solicitação POST para a API com os dados
+resposta = requests.post(url, json=dados)
+# Imprimindo a resposta da API
+print(resposta.json())
+
+#Adicionando Despesa
+url = 'http://127.0.0.1:5000/adicionar-despesa'
+
+# Dados que você quer enviar para a API
+dados = {'valor': 1500}
+
+# Fazendo uma solicitação POST para a API com os dados
+resposta = requests.post(url, json=dados)
+# Imprimindo a resposta da API
+print(resposta.json())
+
+#Definindo um orcamento -> Orçamento é uma váriavel global, ele não é salvo no banco de dados.O orçamento sem vai precisar ser definido. Despes e Receita são salvos no banco de dados.
+url = 'http://127.0.0.1:5000/adicionar-orcamento'
+
+# Dados que você quer enviar para a API
+dados = {'valor': 5000}
 
 # Fazendo uma solicitação POST para a API com os dados
 resposta = requests.post(url, json=dados)
@@ -12,7 +34,6 @@ resposta = requests.post(url, json=dados)
 print(resposta.json())
 
 #Obtendo o resumo das Finanças
-
 url = 'http://127.0.0.1:5000/obter-resumo'
 
 #Fazendo um solicitação GET para a API
