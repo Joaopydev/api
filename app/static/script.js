e8959107aee9ebda695ade64b22d34c3c1577755
JoaoRibeiro01 <158493175+Joaopydev@users.noreply.github.com>
2024-07-11 12:43:01 -0300

    copy link

--- app/static/script.js
+++ app/static/script.js
@@ -5,3 +5,59 @@ setTimeout(function() {
         alert.style.display = 'none'; // Esconde o alerta depois que a opacidade for totalmente reduzida
     }, 2000); // Tempo igual ao tempo de transição
 }, 3000);
+
+
+document.getElementById('copyLinkBtn3').addEventListener('click', function() {
+    const linkButton = document.getElementById('linkToCopy4');
+    const link = linkButton.innerText.trim();
+
+    navigator.clipboard.writeText(link).then(function() {
+        alert('Link copiado com sucesso!');
+    }, function(err) {
+        console.error('Erro ao copiar o link: ', err);
+    });
+});
+
+document.getElementById('copyLinkBtn5').addEventListener('click', function() {
+    const linkButton = document.getElementById('linkToCopy5');
+    const link = linkButton.innerText.trim();
+
+    navigator.clipboard.writeText(link).then(function() {
+        alert('Link copiado com sucesso!');
+    }, function(err) {
+        console.error('Erro ao copiar o link: ', err);
+    });
+});
+
+document.getElementById('copyLinkBtn4').addEventListener('click', function() {
+    const linkButton = document.getElementById('linkToCopy3');
+    const link = linkButton.innerText.trim();
+
+    navigator.clipboard.writeText(link).then(function() {
+        alert('Link copiado com sucesso!');
+    }, function(err) {
+        console.error('Erro ao copiar o link: ', err);
+    });
+});
+
+document.getElementById('copyLinkBtn2').addEventListener('click', function() {
+    const linkButton = document.getElementById('linkToCopy2');
+    const link = linkButton.innerText.trim();
+
+    navigator.clipboard.writeText(link).then(function() {
+        alert('Link copiado com sucesso!');
+    }, function(err) {
+        console.error('Erro ao copiar o link: ', err);
+    });
+});
+
+document.getElementById('copyLinkBtn').addEventListener('click', function() {
+    const linkButton = document.getElementById('linkToCopy');
+    const link = linkButton.innerText.trim();
+
+    navigator.clipboard.writeText(link).then(function() {
+        alert('Link copiado com sucesso!');
+    }, function(err) {
+        console.error('Erro ao copiar o link: ', err);
+    });
+});
